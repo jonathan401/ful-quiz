04d3a0a5d815c1c5982c2f6bf3757f042a48ef8e
jonathan401 <jonathangomina5049@gmail.com>
2024-01-13 17:27:00 +0100

    add function to shuffle choices too

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,32 @@
 import { QuizAnswerType, QuizQuestionType } from "../types/quiz";
 
-export const shuffleArray = (array: QuizQuestionType[]) => {
-  const shuffledArray = array
-    .map((a) => ({ sort: Math.random(), value: a }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value);
+// export const shuffleArray = (array: QuizQuestionType[]) => {
+//   const shuffledArray = array
+//     .map((a) => ({ sort: Math.random(), value: a }))
+//     .sort((a, b) => a.sort - b.sort)
+//     .map((a) => a.value);
 
-  // console.log(shuffledArray.map((question) => question.choices));
+//   // console.log(shuffledArray.map((question) => question.choices));
+//   return shuffledArray;
+// };
+
+export const shuffleArray = (array: QuizQuestionType[]) => {
+  const shuffledArray = [...array];
+  shuffledArray.forEach((question) => {
+    for (let i = question.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [question.choices[i], question.choices[j]] = [
+        question.choices[j],
+        question.choices[i],
+      ];
+    }
+  });
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
   return shuffledArray;
 };
 
